fcfbfdc0b61050afccb8485691711d389b3c2b65
MohammadAyubAnsari <mdayubansari2014@gmail.com>
2024-02-25 11:28:00 +0530

    Database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
+mongoose
+  .connect(process.env.MONGO as string)
+  .then(() => console.log("Connected to database!"));
 const app = express();
 app.use(express.json());
 app.use(cors());
