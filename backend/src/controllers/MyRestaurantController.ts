10b528213a3954343ac78b75cf982b36e398d5a9
MohammadAyubAnsari <mdayubansari2014@gmail.com>
2024-03-09 18:44:30 +0530

    Get My Restaurant API

--- backend/src/controllers/MyRestaurantController.ts
+++ backend/src/controllers/MyRestaurantController.ts
@@ -3,6 +3,20 @@ import Restaurant from "../model/restaurant";
 import cloudinary from "cloudinary";
 import mongoose from "mongoose";
 
+const getMyRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findOne({ user: req.userId });
+    if (!restaurant) {
+      return res.status(404).json({ message: "restaurant not found" });
+    }
+
+    res.json(restaurant);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Error fetching restaurant" });
+  }
+};
+
 const createMyRestaurant = async (req: Request, res: Response) => {
   try {
     const existingRestaurant = await Restaurant.findOne({ user: req.userId });
@@ -32,5 +46,6 @@ const createMyRestaurant = async (req: Request, res: Response) => {
 };
 
 export default {
+  getMyRestaurant,
   createMyRestaurant,
 };
