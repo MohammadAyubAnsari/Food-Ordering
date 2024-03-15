604d939950d16fb37184b5e6db195bcd78763b3c
MohammadAyubAnsari <mdayubansari2014@gmail.com>
2024-03-15 15:57:54 +0530

    Create Session API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import myUserRoute from "./routes/MyUserRoutes";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import { v2 as cloudinary } from "cloudinary";
 import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 
 mongoose
   .connect(process.env.MONGO as string)
@@ -29,6 +30,7 @@ app.get("/health", async (req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurants", restaurantRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(3000, () => {
   console.log("Server started on localhost:3000");
