3a163c7cece643805c5b19f309a53e4186dd61bd
MohammadAyubAnsari <mdayubansari2014@gmail.com>
2024-03-26 23:55:08 +0530

    Update order API

--- backend/src/controllers/MyRestaurantController.ts
+++ backend/src/controllers/MyRestaurantController.ts
@@ -99,6 +99,32 @@ const getMyRestaurantOrders = async (req: Request, res: Response) => {
   }
 };
 
+const updateOrderStatus = async (req: Request, res: Response) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+
+    const order = await Order.findById(orderId);
+    if (!order) {
+      return res.status(404).json({ message: "order not found" });
+    }
+
+    const restaurant = await Restaurant.findById(order.restaurant);
+
+    if (restaurant?.user?._id.toString() !== req.userId) {
+      return res.status(401).send();
+    }
+
+    order.status = status;
+    await order.save();
+
+    res.status(200).json(order);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Unable to update order status" });
+  }
+};
+
 const uploadImage = async (file: Express.Multer.File) => {
   const image = file;
   const base64Image = Buffer.from(image.buffer).toString("base64");
@@ -113,4 +139,5 @@ export default {
   createMyRestaurant,
   updateMyRestaurant,
   getMyRestaurantOrders,
+  updateOrderStatus,
 };
